35130907f44272edf58363e8fc978e4c72839575
msmichellegar <msmichellegar@gmail.com>
2015-10-13 02:36:06 +0100

    playing with lazyload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 $(".hamburger").click(function() {
     $(".hamburger").css("display", "none");
     $(".close").css("display", "block");
-    $(".menu").slideDown();
+    $(".menu").animate({
+        top: "0px"
+    }, 200);
 });
 
 $(".close").click(function() {
     $(".close").css("display", "none");
     $(".hamburger").css("display", "block");
-    $(".menu").slideUp();
+    $(".menu").animate({
+        top: "-50px"
+    }, 600);
 });
 
 $(".profile").hover(function() {
     $(".profile-text").fadeIn("slow");
 })
+
+$(function() {
+    $("img.lazy").lazyload({
+    });
+});
